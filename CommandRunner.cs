3d1f76fa6d3c2ce5cef4be2ae331637d6fe6830d
Anthony Steele <Anthony@PlasticAvalon.org>
2024-08-11 16:15:05 +0100

    Initial commit

---
+++ CommandRunner.cs
@@ -0,0 +1,24 @@
+﻿using Spectre.Console;
+using System.Diagnostics;
+
+namespace HuskyInstaller;
+
+public class CommandRunner(string path)
+{
+    public void RunCommand(string command)
+    {
+        AnsiConsole.MarkupLine($"[green]{command}[/]");
+
+        Process process = new();
+        process.StartInfo.FileName = "cmd.exe";
+        process.StartInfo.WorkingDirectory = path;
+        process.StartInfo.Arguments = "/c " + command;
+        process.StartInfo.RedirectStandardOutput = true;
+
+        process.Start();
+        process.WaitForExit();
+
+        string output = process.StandardOutput.ReadToEnd();
+        Console.WriteLine(output);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using HuskyInstaller;
+using Spectre.Console;
+
+internal class Program
+{
+    private static void RunSetupCommandsInPath(string path)
+    {
+        var runner = new CommandRunner(path);
+
+        runner.RunCommand("dotnet new tool-manifest");
+        runner.RunCommand("dotnet tool install husky");
+        runner.RunCommand("dotnet tool restore");
+        runner.RunCommand("dotnet husky install");
+        runner.RunCommand("dotnet husky add pre-commit -c \"dotnet husky run\"");
+    }
+
+    private static void Main()
+    {
+        var basePath = Directory.GetCurrentDirectory();
+        AnsiConsole.Markup("Husky Installer");
+
+        AnsiConsole.MarkupLine($"Working in path [blue]{basePath}[/]");
+
+        var gitPath = Path.Combine(basePath, ".git");
+
+
+        if (! Directory.Exists(gitPath))
+        {
+            AnsiConsole.MarkupLine($"[red]{gitPath} not found, not a git repo?[/]");
+            return;
+        }
+
+        var taskRunnerFile = Path.Combine(basePath, ".husky", "task-runner.json");
+
+        if (File.Exists(taskRunnerFile))
+        {
+            AnsiConsole.MarkupLine($"Task runner file at [red]{taskRunnerFile}[/] already exists");
+            return;
+        }
+        AnsiConsole.MarkupLine($"Task runner file at [blue]{taskRunnerFile}[/] does not exist");
+
+        RunSetupCommandsInPath(basePath);
+
+        const string taskRunnerData =
+            """
+            {
+               "tasks": [
+                  {
+                     "name": "dotnet-format",
+                     "group": "pre-commit",
+                     "command": "dotnet",
+                     "args": ["format","--no-restore", "--include", "${staged}"],
+                     "include": ["**/*.cs"]
+                  }
+               ]
+            }
+            """;
+        File.WriteAllText(taskRunnerFile, taskRunnerData.Trim());
+
+        AnsiConsole.MarkupLine("Done");
+    }
+}
